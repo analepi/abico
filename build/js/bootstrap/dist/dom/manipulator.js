f0e816b66d0dae48adbf06bd389f73ab7c395961
Analepi <a.g.lepikhova@gmail.com>
2023-06-24 14:31:37 +0300

    мой первый коммит

---
+++ build/js/bootstrap/dist/dom/manipulator.js
@@ -0,0 +1,6 @@
+/*!
+  * Bootstrap manipulator.js v5.3.0 (https://getbootstrap.com/)
+  * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+  */
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e():"function"==typeof define&&define.amd?define(e):(t="undefined"!=typeof globalThis?globalThis:t||self).Manipulator=e()}(this,(function(){"use strict";function t(t){if("true"===t)return!0;if("false"===t)return!1;if(t===Number(t).toString())return Number(t);if(""===t||"null"===t)return null;if("string"!=typeof t)return t;try{return JSON.parse(decodeURIComponent(t))}catch(e){return t}}function e(t){return t.replace(/[A-Z]/g,(t=>`-${t.toLowerCase()}`))}return{setDataAttribute(t,r,n){t.setAttribute(`data-bs-${e(r)}`,n)},removeDataAttribute(t,r){t.removeAttribute(`data-bs-${e(r)}`)},getDataAttributes(e){if(!e)return{};const r={},n=Object.keys(e.dataset).filter((t=>t.startsWith("bs")&&!t.startsWith("bsConfig")));for(const i of n){let n=i.replace(/^bs/,"");n=n.charAt(0).toLowerCase()+n.slice(1,n.length),r[n]=t(e.dataset[i])}return r},getDataAttribute:(r,n)=>t(r.getAttribute(`data-bs-${e(n)}`))}}));
---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('gulp-autoprefixer'),
+    terser = require('gulp-terser'),
+    rigger = require('gulp-rigger'),
+    //rimraf = require('rimraf'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+const path = {
+    build:{
+        html:'build/',
+        js:'build/js/',
+        scss:'build/css/',
+        img:'build/img/'
+    },
+    src:{
+        html:'src/*.{html,htm}',
+        js:'src/js/**/*.js',
+        scss:'src/scss/style.scss',
+        img:'src/img/**/*.{png,jpg,gif,webp,svg}'
+    },
+    watch:{
+        html:'src/*.{html,htm}',
+        js:'src/js/**/*.js',
+        scss:'src/scss/style.scss',
+        img:'src/img/**/*.{png,jpg,gif,webp,svg}'
+    },
+    clean:'build/'
+};
+
+const config = {
+    server: {
+        baseDir: "./build", // base directory
+        index: "index.html", // start page
+    },
+    tunnel: true, // tunnel
+    host: 'localhost',
+    port: 7787,
+    logPrefix: "WebDev"
+};
+
+// gulp.task('clean', function (done) {
+//     rimraf(path.clean, done);
+// });
+
+gulp.task('build:html', function (done){
+    gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream:true}));
+    done();
+});
+
+gulp.task('mv:img', function (done){
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream:true}));
+    done();
+});
+
+gulp.task('build:js', function (done){
+    gulp.src(path.src.js)
+        .pipe(plumber())
+        .pipe(terser())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream:true}));
+    done();
+});
+
+gulp.task('build:scss', function (done){
+    gulp.src(path.src.scss)
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.build.scss))
+        .pipe(reload({stream:true}));
+    done();
+});
+
+gulp.task('dev:scss', function (done){
+    gulp.src(path.src.scss, {sourcemaps:true})
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.build.scss, {sourcemaps:'.'}))
+        .pipe(reload({stream:true}));
+    done();
+});
+
+gulp.task('watch', function (done){
+    gulp.watch(path.watch.html, gulp.series('build:html'));
+    gulp.watch(path.watch.scss, gulp.series('build:scss'));
+    gulp.watch(path.watch.img, gulp.series('mv:img'));
+    done();
+});
+
+gulp.task('webserver',function (done){
+    browserSync.init(config);
+    done();
+})
+
+gulp.task('build', gulp.parallel('build:html','build:js','build:scss','mv:img'));
+gulp.task('dev', gulp.parallel('build:html','build:js','dev:scss','mv:img'));
+gulp.task('default', gulp.series('build','watch','webserver'));
